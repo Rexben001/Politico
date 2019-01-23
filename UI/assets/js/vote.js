b2f70e61e7e4df890b62242bda231e1ec0549463
Rexben <rexben.rb@gmail.com>
2019-01-23 21:08:15 +0100

    [Feature #163312268] Created side bar menu

--- UI/assets/js/vote.js
+++ UI/assets/js/vote.js
@@ -10,4 +10,48 @@ document.getElementById('office').addEventListener('change', (e) => {
         <option value='obi'>Oby Ezekweseli</option><br>
         <option value='mog'>Ali Moghalu</option>`
     }
-})
+});
+
+const vote = document.getElementById('votes');
+const parties = document.getElementById('parties');
+const candidate = document.getElementById('candy');
+
+const votes = document.getElementById('vote');
+const pol = document.getElementById('pol');
+const candidates = document.getElementById('candidates');
+
+
+
+document.getElementById('vote').addEventListener('click', (e) => {
+    e.preventDefault();
+    vote.style.display = 'block';
+    parties.style.display = 'none';
+    candidate.style.display = 'none';
+});
+
+
+document.getElementById('pol').addEventListener('click', (e) => {
+    e.preventDefault();
+    vote.style.display = 'none';
+    parties.style.display = 'block';
+    candidate.style.display = 'none';
+});
+
+document.getElementById('candidates').addEventListener('click', (e) => {
+    e.preventDefault();
+    vote.style.display = 'none';
+    parties.style.display = 'none';
+    candidate.style.display = 'block';
+});
+
+// const showHide = (vote, votes, party, can) => {
+//     document.getElementById(votes).addEventListener('click', () => {
+
+//         vote.style.display = 'block';
+//         party.style.display = 'none';
+//         can.style.display = 'none';
+//     });
+
+// }
+// showHide(vote, votes, parties, candidate);
+// showHide(parties, pol, vote, candidate);
