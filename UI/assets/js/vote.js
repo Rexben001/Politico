21d6acb393103c0902675037310f69be32c3b06a
Rexben <rexben.rb@gmail.com>
2019-01-23 22:00:40 +0100

    [Feature #163312268] Updated the select list to be flexible

--- UI/assets/js/vote.js
+++ UI/assets/js/vote.js
@@ -2,13 +2,40 @@ document.getElementById('office').addEventListener('change', (e) => {
     e.preventDefault();
     const val = document.getElementById('office');
     const newVal = val.options[val.selectedIndex].value;
+    const populate = document.getElementById('disabled');
     if (newVal == 'pre') {
-        const populate = document.getElementById('disabled');
-        populate.innerHTML += `&nbsp;<option value='buh'>Mohammed Buhari</option>&nbsp;
+        populate.innerHTML += `<option value='buh'>Mohammed Buhari</option>
         <option value='ati'>Atiku Abubakar</option><br>
         <option value='sow'>James Sowore</option><br>
         <option value='obi'>Oby Ezekweseli</option><br>
         <option value='mog'>Ali Moghalu</option>`
+    }
+    else if (newVal == 'sen') {
+        populate.innerHTML += `<option value='dino'>Dino Melaye</option>
+        <option value='tin'>Oluremi Tinubu</option><br>
+        <option value='sara'>Saraki Danjuma</option><br>
+        <option value='ekwe'>Ekweremadu James</option><br>
+        <option value='femi'>Femi Gbajamila</option>`
+    }
+    else if (newVal == 'hop') {
+        populate.innerHTML += `<option value='dino'>Sadiq Ibrahim</option>
+        <option value='tin'>Samuel Ikon</option><br>
+        <option value='sara'>Owoidighe Ekpoatai</option><br>
+        <option value='ekwe'>Okon Michael</option><br>`
+    }
+    else if (newVal == 'gov') {
+        populate.innerHTML += `<option value='dino'>Okezie Ikpeazu</option>
+        <option value='tin'>Bindo Jibrilla</option><br>
+        <option value='sara'>Udom Gabriel Emmanuel</option><br>
+        <option value='ekwe'>Willie Obiano</option><br>
+        <option value='femi'>Henry Dickson</option>`
+    }
+    else if (newVal == 'lgc') {
+        populate.innerHTML += `<option value='dino'>Ndumati E.Lawson Ndu</option>
+        <option value='tin'>John Naale</option><br>
+        <option value='sara'>Timothy E. Nsirim</option><br>
+        <option value='ekwe'>L.W. Chukwu</option><br>
+        <option value='femi'>Adeniji Jones</option>`
     }
 });
 
