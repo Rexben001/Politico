d991ba7c552fdffe442a793511b04d0b1d3b860b
Rexben <rexben.rb@gmail.com>
2019-01-23 14:57:47 +0100

    [Feature #163312268] FIxed the nav bar

--- UI/assets/js/vote.js
+++ UI/assets/js/vote.js
@@ -4,10 +4,10 @@ document.getElementById('office').addEventListener('change', (e) => {
     const newVal = val.options[val.selectedIndex].value;
     if (newVal == 'pre') {
         const populate = document.getElementById('disabled');
-        populate.innerHTML += `<option id='buh'>Mohammed Buhari</option><br>
-        <option id='ati'>Atiku Abubakar</option><br>
-        <option id='sow'>James Sowore</option><br>
-        <option id='obi'>Oby Ezekweseli</option><br>
-        <option id='mog'>Ali Moghalu</option>`
+        populate.innerHTML += `&nbsp;<option value='buh'>Mohammed Buhari</option>&nbsp;
+        <option value='ati'>Atiku Abubakar</option><br>
+        <option value='sow'>James Sowore</option><br>
+        <option value='obi'>Oby Ezekweseli</option><br>
+        <option value='mog'>Ali Moghalu</option>`
     }
 })
