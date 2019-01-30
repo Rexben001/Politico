4530baead067c8982e9b0eb8fba25ddddfbcb49e
rexben.rb@gmail.com <rexben.rb@gmail.com>
2019-01-30 07:29:56 +0100

    [Chore #163498857] Imported express and body-parser and create a homepage router

---
+++ server/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: false,
+}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.status(200).json({
+    status: 200,
+    message: 'Politico Xpress',
+}));
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log('Working');
+});
