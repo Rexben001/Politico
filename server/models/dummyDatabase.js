c1a540d3a54ea97ae49cf29bf6ad69c0557c5b35
rexben.rb@gmail.com <rexben.rb@gmail.com>
2019-01-30 11:19:40 +0100

    [Feature #163500312] Create a dummy database for parties

---
+++ server/models/dummyDatabase.js
@@ -0,0 +1,14 @@
+const parties = [
+    {
+        party_id: 1,
+        name: 'Action People (AP)',
+        hqAddress: '10, Allison Street, Lagos',
+        logoUrl: 'https://politico.com/ap_logo'
+    },
+    {
+        party_id: 2,
+        name: 'Precious People Party (PPP)',
+        hqAddress: 'Block 77, Aso Rock Villa, Abuja',
+        logoUrl: 'https://politico.com/ap_logo'
+    }
+];
