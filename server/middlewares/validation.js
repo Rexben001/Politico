0dfd80f21de5622689281edabac3c264907730b3
rexben.rb@gmail.com <rexben.rb@gmail.com>
2019-01-30 13:09:19 +0100

    [Feature #163500312] Validate the register party inputs

---
+++ server/middlewares/validation.js
@@ -0,0 +1,26 @@
+import validate from 'node-input-validator';
+class Validation {
+
+    static partyValidator(req, res, next) {
+
+        let validator = new validate(req.body, {
+            party_id: 'required|integer',
+            name: 'required|minLength:5',
+            hqAddress: 'required|string|minLength:10',
+            logoUrl: 'required|string'
+        });
+
+        validator.check().then(function (matched) {
+            if (!matched) {
+                return res.status(422).json({
+                    "status": 422,
+                    "error": 'Unable to process your request, make sure the fields are entered correctly'
+                });
+            }
+            next();
+        });
+
+    }
+}
+
+export default Validation;
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import validator from '../middlewares/validation';
 import AdminController from '../controllers/adminController';
 
 const router = express.Router();
 
-router.post('/parties', AdminController.registerParty);
+router.post('/parties', validator.partyValidator, AdminController.registerParty);
 
 export default router;
