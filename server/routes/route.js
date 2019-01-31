cd1f144ed3ac0e3df2303944292f34ae4ec73f57
rexben.rb@gmail.com <rexben.rb@gmail.com>
2019-01-31 13:15:36 +0100

    [Feature #163500861] Create delete party route

--- server/routes/route.js
+++ server/routes/route.js
@@ -8,6 +8,8 @@ router.post('/parties', validator.partyValidator, AdminController.registerParty)
 router.get('/parties', AdminController.getAllParties);
 router.get('/parties/:party_id', AdminController.getOneParty);
 router.patch('/parties/:party_id/name', validator.editPartyValidator, AdminController.editOneParty);
+router.delete('/parties/:party_id', AdminController.deleteOneParty);
+
 
 
 
