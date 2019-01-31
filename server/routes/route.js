e2bacdaeb37ff2dcad2dc02949156a32293a4f2c
rexben.rb@gmail.com <rexben.rb@gmail.com>
2019-01-31 16:25:34 +0100

    [Feature #163500794] Create routes for get all office and for get an office

--- server/routes/route.js
+++ server/routes/route.js
@@ -11,6 +11,8 @@ router.patch('/parties/:party_id/name', validator.editPartyValidator, AdminContr
 router.delete('/parties/:party_id', AdminController.deleteOneParty);
 
 router.post('/offices', validator.officeValidator, AdminController.registerOffice);
+router.get('/offices', AdminController.getAllOffices);
+router.get('/offices/:office_id', AdminController.getOneOffice);
 
 
 export default router;
