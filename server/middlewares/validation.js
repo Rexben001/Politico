5526d0122f96736a6a35b80998232f344bc8ba5d
rexben.rb@gmail.com <rexben.rb@gmail.com>
2019-02-01 10:59:05 +0100

    [Chore #163501227] Add a script to run eslint

--- server/middlewares/validation.js
+++ server/middlewares/validation.js
@@ -1,68 +1,62 @@
 import validate from 'node-input-validator';
+
 class Validation {
+  static partyValidator(req, res, next) {
+    const validator = new validate(req.body, {
+      party_id: 'required|integer',
+      name: 'required|minLength:5',
+      hqAddress: 'required|string|minLength:10',
+      logoUrl: 'required|string'
+    });
 
-    static partyValidator(req, res, next) {
-
-        let validator = new validate(req.body, {
-            party_id: 'required|integer',
-            name: 'required|minLength:5',
-            hqAddress: 'required|string|minLength:10',
-            logoUrl: 'required|string'
+    validator.check().then((matched) => {
+      if (!matched) {
+        return res.status(400).json({
+          status: 400,
+          error: 'Unable to process your request, make sure the fields are entered correctly'
         });
+      }
+      next();
+    });
+  }
 
-        validator.check().then(function (matched) {
-            if (!matched) {
-                return res.status(400).json({
-                    "status": 400,
-                    "error": 'Unable to process your request, make sure the fields are entered correctly'
-                });
-            }
-            next();
+
+  static editPartyValidator(req, res, next) {
+    const validator = new validate(req.body, {
+      name: 'required|minLength:5',
+      hqAddress: 'required|string|minLength:10',
+      logoUrl: 'required|string'
+    });
+
+    validator.check().then((matched) => {
+      if (!matched) {
+        return res.status(400).json({
+          status: 400,
+          error: 'Unable to process your request, make sure the fields are entered correctly'
         });
+      }
+      next();
+    });
+  }
 
-    }
+  static officeValidator(req, res, next) {
+    const validator = new validate(req.body, {
+      office_id: 'required|integer',
+      type: 'required|string',
+      name: 'required|string',
+      region: 'required|string'
+    });
 
-
-    static editPartyValidator(req, res, next) {
-
-        let validator = new validate(req.body, {
-            name: 'required|minLength:5',
-            hqAddress: 'required|string|minLength:10',
-            logoUrl: 'required|string'
+    validator.check().then((matched) => {
+      if (!matched) {
+        return res.status(400).json({
+          status: 400,
+          error: 'Unable to process your request, make sure the fields are entered correctly'
         });
-
-        validator.check().then(function (matched) {
-            if (!matched) {
-                return res.status(400).json({
-                    "status": 400,
-                    "error": 'Unable to process your request, make sure the fields are entered correctly'
-                });
-            }
-            next();
-        });
-    }
-
-    static officeValidator(req, res, next) {
-
-        let validator = new validate(req.body, {
-            office_id: 'required|integer',
-            type: 'required|string',
-            name: 'required|string',
-            region: 'required|string'
-        });
-
-        validator.check().then(function (matched) {
-            if (!matched) {
-                return res.status(400).json({
-                    "status": 400,
-                    "error": 'Unable to process your request, make sure the fields are entered correctly'
-                });
-            }
-            next();
-        });
-
-    }
-
+      }
+      next();
+    });
+  }
 }
 
 export default Validation;
--- server/specs/app.js
+++ server/specs/app.js
@@ -7,13 +7,13 @@ chai.should();
 chai.use(chaiHttp);
 
 describe('GET /', () => {
-    it('it should get homepage', ((done) => {
-        chai.request(app)
-            .get('/')
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.have.property('message').equal('Politico Xpress');
-                done(err);
-            });
-    }));
+  it('it should get homepage', ((done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property('message').equal('Politico Xpress');
+        done(err);
+      });
+  }));
 });
