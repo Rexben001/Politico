865c70006ec5b44aa4e589575868baec02e307f4
rexben.rb@gmail.com <rexben.rb@gmail.com>
2019-02-01 10:27:12 +0100

    Removed send

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -30,7 +30,7 @@ class AdminController {
     }
 
     static getAllParties(req, res) {
-        return res.status(200).send({
+        return res.status(200).json({
             "status": 200,
             "data": parties
         });
