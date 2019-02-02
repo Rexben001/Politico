c7f3622623a8425a905b3c430c5f999042db1a38
rexben.rb@gmail.com <rexben.rb@gmail.com>
2019-02-02 20:56:32 +0100

    [Chore #163501227] Add office validator

--- server/middlewares/validation.js
+++ server/middlewares/validation.js
@@ -1,63 +1,68 @@
 import validate from 'node-input-validator';
-/**
- *
- * @class Validation
- */
 class Validation {
-    /**
-           *
-           *
-           * @static
-           * @param {*} req
-           * @param {*} res
-           * @param {*} next
-           * @memberof Validation
-           */
+
     static partyValidator(req, res, next) {
-        const validator = new validate(req.body, {
+
+        let validator = new validate(req.body, {
             party_id: 'required|integer',
             name: 'required|string|minLength:5',
             hqAddress: 'required|string|minLength:10',
             logoUrl: 'required|string'
         });
 
-        validator.check().then((matched) => {
+        validator.check().then(function (matched) {
             if (!matched) {
                 return res.status(400).json({
-                    status: 400,
-                    error: validator.errors
+                    "status": 400,
+                    "error": validator.errors
                 });
             }
             next();
         });
+
     }
 
-    /**
-           *
-           *
-           * @static
-           * @param {*} req
-           * @param {*} res
-           * @param {*} next
-           * @memberof Validation
-           */
+
     static editPartyValidator(req, res, next) {
-        const validator = new validate(req.body, {
+
+        let validator = new validate(req.body, {
             name: 'required|minLength:5',
             hqAddress: 'required|string|minLength:10',
             logoUrl: 'required|string'
         });
 
-        validator.check().then((matched) => {
+        validator.check().then(function (matched) {
             if (!matched) {
                 return res.status(400).json({
-                    status: 400,
-                    error: validator.errors
+                    "status": 400,
+                    "error": validator.errors
                 });
             }
             next();
         });
     }
+
+    static officeValidator(req, res, next) {
+
+        let validator = new validate(req.body, {
+            office_id: 'required|integer',
+            type: 'required|string',
+            name: 'required|string',
+            region: 'required|string'
+        });
+
+        validator.check().then(function (matched) {
+            if (!matched) {
+                return res.status(400).json({
+                    "status": 400,
+                    "error": validator.errors
+                });
+            }
+            next();
+        });
+
+    }
+
 }
 
 export default Validation;
