14a061f560a31de7a89eb822246f1547ad7fc452
rexben.rb@gmail.com <rexben.rb@gmail.com>
2019-02-02 21:11:44 +0100

    [Feature #163501096] Create table for users

--- server/models/database.js
+++ server/models/database.js
@@ -19,3 +19,30 @@ pool.on('connect', () => {
     console.log('connected to the Database');
 });
 
+
+const users = async () => {
+    const userTable = `
+    CREATE TABLE IF NOT EXISTS 
+    users(
+        user_id SERIAL PRIMARY KEY,
+        firstname VARCHAR(128) NOT NULL,
+        lastname VARCHAR(128) NOT NULL,
+        othernames VARCHAR(128) NOT NULL,
+        email VARCHAR(128) NOT NULL,
+        password VARCHAR(128) NOT NULL,
+        username VARCHAR(128) NOT NULL,
+        phoneNumber VARCHAR(128) NOT NULL,
+        is_admin BOOLEAN NOT NULL,
+        passportUrl VARCHAR(128) NOT NULL,
+        UNIQUE(username, email)
+        );`;
+    await pool.query(userTable)
+        .then((res) => {
+            // console.log('users table created!: ', res);
+        }).catch((err) => {
+            console.log('An error occured while creating users table: ', err);
+            pool.end();
+        });
+};
+
+export default { users };
