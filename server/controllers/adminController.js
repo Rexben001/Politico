3b334cd71f8d0b3a1cad87b8b921c5e090296403
rexben.rb@gmail.com <rexben.rb@gmail.com>
2019-02-03 22:06:12 +0100

    [Feature #163683601] Create method to delete a party

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -32,7 +32,7 @@ class AdminController {
           if (error || result.rowCount === 0) {
             return res.status(400).json({ status: 400, error: error.detail });
           }
-          res.status(201).json({
+          return res.status(201).json({
             status: 201,
             data: [{
               id: result.rows[0].party_id,
@@ -62,24 +62,17 @@ class AdminController {
         const query = 'SELECT * FROM parties';
         client.query(query, (error, result) => {
           done();
-          if (error) {
-            res.status(500).json({ status: 500, message: `An error occured while trying to get parties, ${error}` });
-          } else {
-            if (result.rowCount === 0) {
-              res.status(500).json({ staus: 500, message: 'The list of parties could not be fetched' });
-            }
-            res.status(200).json({
-              status: 200,
-              data: result.rows
-            });
+          if (error || result.rowCount === 0) {
+            return res.status(404).json({ staus: 404, message: 'The list of parties could not be fetched' });
           }
+          return res.status(200).json({
+            status: 200,
+            data: result.rows
+          });
         });
       });
     } catch (error) {
-      return res.status(400).json({
-        status: 400,
-        error
-      });
+      return res.status(500).json({ status: 500, error: 'Server error' });
     }
   }
 
@@ -100,28 +93,21 @@ class AdminController {
         const query = `SELECT * FROM parties WHERE party_id=${id}`;
         client.query(query, (error, result) => {
           done();
-          if (error) {
-            res.status(500).json({ status: 500, message: `An error occured while trying to get a party, ${error}` });
-          } else {
-            if (result.rowCount === 0) {
-              res.status(500).json({ staus: 500, message: 'Party could not be fetched' });
-            }
-            res.status(200).json({
-              status: 200,
-              data: [{
-                id: result.rows[0].party_id,
-                name: result.rows[0].name,
-                logoUrl: result.rows[0].logourl
-              }]
-            });
+          if (error || result.rowCount === 0) {
+            return res.status(404).json({ staus: 404, message: 'The list of parties could not be fetched' });
           }
+          return res.status(200).json({
+            status: 200,
+            data: [{
+              id: result.rows[0].party_id,
+              name: result.rows[0].name,
+              logoUrl: result.rows[0].logourl
+            }]
+          });
         });
       });
     } catch (error) {
-      return res.status(400).json({
-        status: 400,
-        error
-      });
+      return res.status(500).json({ status: 500, error: 'Server error' });
     }
   }
 
@@ -138,33 +124,58 @@ class AdminController {
     try {
       const id = Number(req.params.party_id);
       const { name, hqAddress, logoUrl } = req.body;
-      try {
-        pool.connect((err, client) => {
-          const query = 'UPDATE parties SET name=$1, hqAddress=$2, logoUrl=$3 WHERE party_id=$4 RETURNING *';
-          const value = [name, hqAddress, logoUrl, id];
-          client.query(query, value, (error, result) => {
-            if (error) {
-              res.status(500).json({ status: 500, message: `An error occured while trying to updating a party, ${error}` });
-            } else {
-              if (result.rowCount === 0) {
-                res.status(500).json({ staus: 500, message: 'Party could not be updated' });
-              }
-              res.status(200).json({
-                status: 200,
-                data: [{
-                  id: result.rows[0].party_id,
-                  name: result.rows[0].name
-                }]
-              });
-            }
+      pool.connect((err, client, done) => {
+        const query = 'UPDATE parties SET name=$1, hqAddress=$2, logoUrl=$3 WHERE party_id=$4 RETURNING *';
+        const value = [name, hqAddress, logoUrl, id];
+        client.query(query, value, (error, result) => {
+          done();
+          if (error || result.rowCount === 0) {
+            return res.status(404).json({ staus: 404, message: 'The list of parties could not be fetched' });
+          }
+          return res.status(200).json({
+            status: 201,
+            data: [{
+              id: result.rows[0].party_id,
+              name: result.rows[0].name
+            }]
           });
         });
-      } catch (error) {
-        return res.status(400).json({
-          status: 400,
-          error
+      });
+    } catch (error) {
+      return res.status(500).json({ status: 500, error: 'Server error' });
+    }
+  }
+
+  /**
+   *
+   *
+   * @static
+   * @param {*} req
+   * @param {*} res
+   * @returns
+   * @memberof AdminController
+   */
+  static deleteOneParty(req, res) {
+    try {
+      const id = Number(req.params.party_id);
+      pool.connect((err, client, done) => {
+        if (err) throw err;
+        const query = `DELETE FROM parties WHERE party_id=${id}`;
+        client.query(query, (error, result) => {
+          done();
+          if (error) {
+            res.status(500).json({ status: 500, message: `An error occured while trying to get a party, ${error}` });
+          } else {
+            if (result.rowCount === 0) {
+              res.status(500).json({ staus: 500, message: 'Party could not be fetched' });
+            }
+            res.status(200).json({
+              status: 200,
+              data: result.rows[0]
+            });
+          }
         });
-      }
+      });
     } catch (error) {
       return res.status(400).json({
         status: 400,
@@ -173,22 +184,6 @@ class AdminController {
     }
   }
 
-  // static deleteOneParty(req, res) {
-  //   const id = Number(req.params.party_id);
-  //   const singleParty = parties.find(parties => parties.party_id == id);
-  //   if (!singleParty) {
-  //     return res.status(404).json({
-  //       status: 404,
-  //       error: 'Unable to retrieve party'
-  //     });
-  //   }
-  //   const indexOfSignleParty = parties.indexOf(singleParty);
-  //   parties.splice(indexOfSignleParty, 1);
-  //   return res.status(200).json({
-  //     status: 200,
-  //     data: [{ message: `You have successfully deleted ${singleParty.name}` }]
-  //   });
-  // }
 
   // static registerOffice(req, res) {
   //   const {
