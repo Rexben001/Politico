0ebfb9d97177f310df3217c7a8f4e57b989fe94a
rexben.rb@gmail.com <rexben.rb@gmail.com>
2019-02-03 20:20:48 +0100

    [Feature #163683601] Create route to register party

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import validator from '../middlewares/validation';
 import UserController from '../controllers/userController';
+import AdminController from '../controllers/adminController';
 
 const router = express.Router();
 
-// router.post('/parties', validator.partyValidator, AdminController.registerParty);
+router.post('/parties', validator.partyValidator, AdminController.registerParty);
 // router.get('/parties', AdminController.getAllParties);
 // router.get('/parties/:party_id', AdminController.getOneParty);
 // router.patch('/parties/:party_id/name', validator.editPartyValidator, AdminController.editOneParty);
