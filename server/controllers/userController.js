5fdb26df2266ce04f134b4fc3e9601b6429d044e
rexben.rb@gmail.com <rexben.rb@gmail.com>
2019-02-03 13:38:15 +0100

    [Feature #163501123] Create a method to login users

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -46,6 +46,41 @@ class UserControllers {
       });
     });
   }
+
+  /**
+   *
+   *
+   * @static
+   * @param {*} req
+   * @param {*} res
+   * @memberof UserControllers
+   */
+  static loginUser(req, res) {
+    const { email, password } = req.body;
+    pool.connect((err, client, done) => {
+      if (err) throw err;
+      const query = 'SELECT * FROM users WHERE email=$1 AND password=$2';
+      const value = [email, password];
+      client.query(query, value, (error, result) => {
+        done();
+        if (error) {
+          res.status(500).json({ status: 500, message: 'An error occured while trying to fetch user' });
+        } else {
+          if (result.rowCount === 0) {
+            res.status(500).json({ staus: 500, message: 'The user could not be saved' });
+          }
+          res.status(200).json({
+            status: 200,
+            data: [{
+              token: 34567,
+              user: result.rows[0]
+            }]
+          });
+        }
+      });
+    });
+  }
+
 }
 
 export default UserControllers;
