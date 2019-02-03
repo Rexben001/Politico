06c2e216867eb38a8199539addef499b3fd0f225
rexben.rb@gmail.com <rexben.rb@gmail.com>
2019-02-03 22:43:45 +0100

    [Feature #163683601] Create route to register a new office

--- server/routes/route.js
+++ server/routes/route.js
@@ -11,7 +11,7 @@ router.get('/parties/:party_id', AdminController.getOneParty);
 router.patch('/parties/:party_id/name', validator.editPartyValidator, AdminController.editOneParty);
 router.delete('/parties/:party_id', AdminController.deleteOneParty);
 
-// router.post('/offices', validator.officeValidator, AdminController.registerOffice);
+router.post('/offices', validator.officeValidator, AdminController.registerOffice);
 // router.get('/offices', AdminController.getAllOffices);
 // router.get('/offices/:office_id', AdminController.getOneOffice);
 
