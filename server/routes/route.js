f2e020a575045c92860108eae023dd122f5e044b
rexben.rb@gmail.com <rexben.rb@gmail.com>
2019-02-03 21:08:21 +0100

    [Feature #163683601] Create a route to get all parties

--- server/routes/route.js
+++ server/routes/route.js
@@ -6,7 +6,7 @@ import AdminController from '../controllers/adminController';
 const router = express.Router();
 
 router.post('/parties', validator.partyValidator, AdminController.registerParty);
-// router.get('/parties', AdminController.getAllParties);
+router.get('/parties', AdminController.getAllParties);
 // router.get('/parties/:party_id', AdminController.getOneParty);
 // router.patch('/parties/:party_id/name', validator.editPartyValidator, AdminController.editOneParty);
 // router.delete('/parties/:party_id', AdminController.deleteOneParty);
