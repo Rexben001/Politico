ef277e6072eaf53ca7802deb30fd8788da963d45
rexben.rb@gmail.com <rexben.rb@gmail.com>
2019-02-03 21:24:05 +0100

    [Feature #163683601] Create route to get a specific party

--- server/routes/route.js
+++ server/routes/route.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post('/parties', validator.partyValidator, AdminController.registerParty);
 router.get('/parties', AdminController.getAllParties);
-// router.get('/parties/:party_id', AdminController.getOneParty);
+router.get('/parties/:party_id', AdminController.getOneParty);
 // router.patch('/parties/:party_id/name', validator.editPartyValidator, AdminController.editOneParty);
 // router.delete('/parties/:party_id', AdminController.deleteOneParty);
 
