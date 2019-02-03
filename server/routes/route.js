f861e26361db872273c16bf72d58cefe463ee0e0
rexben.rb@gmail.com <rexben.rb@gmail.com>
2019-02-03 21:41:27 +0100

    [Feature #163683601] Create route to edit a party

--- server/routes/route.js
+++ server/routes/route.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post('/parties', validator.partyValidator, AdminController.registerParty);
 router.get('/parties', AdminController.getAllParties);
 router.get('/parties/:party_id', AdminController.getOneParty);
-// router.patch('/parties/:party_id/name', validator.editPartyValidator, AdminController.editOneParty);
+router.patch('/parties/:party_id/name', validator.editPartyValidator, AdminController.editOneParty);
 // router.delete('/parties/:party_id', AdminController.deleteOneParty);
 
 // router.post('/offices', validator.officeValidator, AdminController.registerOffice);
