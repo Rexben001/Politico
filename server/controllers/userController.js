0eaffcd31db50196e6e7516e61661d4591c7325a
rexben.rb@gmail.com <rexben.rb@gmail.com>
2019-02-03 14:39:36 +0100

    [Feature #163501123] Encrypt user's password

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 import database from '../models/database';
 
 const { pool } = database;
@@ -22,28 +23,34 @@ class UserControllers {
       const {
         firstname, lastname, othernames, username, email, phonenumber, password, passportUrl
       } = req.body;
-      pool.connect((err, client, done) => {
-        if (err) throw err;
-        const query = `INSERT INTO users(firstname, lastname, othernames, username,
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, (err, hash) => {
+          if (err) throw err;
+          const passwordHash = hash;
+          pool.connect((err, client, done) => {
+            if (err) throw err;
+            const query = `INSERT INTO users(firstname, lastname, othernames, username,
                   email, phoneNumber, password, is_admin, passportUrl) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *`;
-        const value = [firstname, lastname, othernames, username,
-          email, phonenumber, password, false, passportUrl];
-        client.query(query, value, (error, result) => {
-          if (error || result.rowCount === 0) {
-            done();
-            return res.status(400).json({ status: 400, error: error.detail });
-          }
-          jwt.sign({ username, password },
-            process.env.SECRETKEY, { expiresIn: '20d' }, (err, token) => {
-              if (err) throw err;
-              res.status(201).json({
-                status: 201,
-                data: [{
-                  token,
-                  user: result.rows[0]
-                }]
-              });
+            const value = [firstname, lastname, othernames, username,
+              email, phonenumber, passwordHash, false, passportUrl];
+            client.query(query, value, (error, result) => {
+              if (error || result.rowCount === 0) {
+                done();
+                return res.status(400).json({ status: 400, error: error.detail });
+              }
+              jwt.sign({ username, password },
+                process.env.SECRETKEY, { expiresIn: '20d' }, (err, token) => {
+                  if (err) throw err;
+                  res.status(201).json({
+                    status: 201,
+                    data: [{
+                      token,
+                      user: result.rows[0]
+                    }]
+                  });
+                });
             });
+          });
         });
       });
     } catch (error) {
@@ -65,28 +72,30 @@ class UserControllers {
       const { email, password } = req.body;
       pool.connect((err, client, done) => {
         if (err) throw err;
-        const query = 'SELECT * FROM users WHERE email=$1 AND password=$2';
-        const value = [email, password];
+        const query = 'SELECT * FROM users WHERE email=$1';
+        const value = [email];
         client.query(query, value, (error, result) => {
           done();
-          if (error) {
-            res.status(500).json({ status: 500, message: 'An error occured while trying to fetch user' });
-          } else {
-            if (result.rowCount === 0) {
-              res.status(500).json({ staus: 500, message: 'The user could not be saved' });
-            }
-            jwt.sign({ email, password },
-              process.env.SECRETKEY, { expiresIn: '7d' }, (err, token) => {
-                if (err) throw err;
-                res.status(201).json({
-                  status: 201,
-                  data: [{
-                    token,
-                    user: result.rows[0]
-                  }]
-                });
-              });
+          if (error || result.rowCount === 0) {
+            return res.status(404).json({ status: 404, error: `User with ${email} does not exists` });
           }
+          bcrypt.compare(password, result.rows[0].password).then((isMatch) => {
+            if (isMatch) {
+              jwt.sign({ email, password },
+                process.env.SECRETKEY, { expiresIn: '7d' }, (err, token) => {
+                  if (err) throw err;
+                  res.status(201).json({
+                    status: 201,
+                    data: [{
+                      token,
+                      user: result.rows[0]
+                    }]
+                  });
+                });
+            } else {
+              return res.status(404).json({ status: 404, error: 'Incorrect password' });
+            }
+          });
         });
       });
     } catch (error) {
